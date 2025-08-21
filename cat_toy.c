4810494b24003bb80c1c9433bb9a2813c30ece1a
ziggysalsa <zelenski.sasha@gmail.com>
2025-08-21 14:35:50 -0700

    initial commit

---
+++ cat_toy.c
@@ -0,0 +1,46 @@
+#include "pico/stdlib.h"
+
+#define IN1 18
+#define IN2 19
+#define IN3 20
+#define IN4 21
+#define DELAY_MS 4           
+#define STEPS_PER_REV 2048  
+
+static const uint PINS[4] = {IN1, IN2, IN3, IN4};
+
+// Define the half-step positions of the motor
+static const uint8_t SEQ[8][4] = {
+    {1,0,0,0},{1,1,0,0},{0,1,0,0},{0,1,1,0},
+    {0,0,1,0},{0,0,1,1},{0,0,0,1},{1,0,0,1}
+};
+
+int main() {
+
+    // Sets up I/O
+    stdio_init_all();
+
+    // Enables pins for use and sets them as outputs
+    for (int i = 0; i < 4; i++) { 
+        gpio_init(PINS[i]); 
+        gpio_set_dir(PINS[i], true); 
+    }
+
+    int step_idx = 0; // keep track of which half-step position we're on
+    int direction = 1; // 1 is forward, -1 is backward
+
+    while (true) {
+
+        // Do one revolution 
+        for (int s = 0; s < STEPS_PER_REV; s++) {
+            for (int i = 0; i < 4; i++) { 
+                gpio_put(PINS[i], SEQ[step_idx][i]);
+            }
+            sleep_ms(DELAY_MS);
+            step_idx = (step_idx + direction) & 7;
+        }
+
+        // Reverse direction
+        direction = -direction;
+    }
+}
