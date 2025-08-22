854d8440f8014a8dc3b1fb0da24b7cf3f940d7d0
ziggysalsa <zelenski.sasha@gmail.com>
2025-08-22 10:54:27 -0700

    add random pause

--- cat_toy.c
+++ cat_toy.c
@@ -4,9 +4,12 @@
 #define IN1 18
 #define IN2 19
 #define IN3 20
-#define IN4 21
-#define DELAY_MS 4           
+#define IN4 21           
 #define STEPS_PER_REV 2048  
+#define STEP_DELAY_MS 4
+#define PAUSE_PROBABILITY 3
+#define PAUSE_LOWER_BOUND_MS 1000
+#define PAUSE_UPPER_BOUND_MS 5000
 
 static const uint PINS[4] = {IN1, IN2, IN3, IN4};
 
@@ -34,13 +37,19 @@ int main() {
 
         int num_steps = rand() % (STEPS_PER_REV + 1);  // want number of steps to always be fewer than a full revolution
 
-        // Go the random number of steps
+        // Go a random number of steps
         for (int i = 0; i < num_steps; i++) {
             for (int j = 0; j < 4; j++) { 
                 gpio_put(PINS[j], SEQ[step_idx][j]);
             }
-            sleep_ms(DELAY_MS);
+            sleep_ms(STEP_DELAY_MS);
             step_idx = (step_idx + direction) & 7; // proceed to next step
+        }
+
+        // Randomly pause for a random length of time
+        if ((rand() % PAUSE_PROBABILITY) == 0) {
+            int pause_ms = PAUSE_LOWER_BOUND_MS + (rand() % (PAUSE_UPPER_BOUND_MS-PAUSE_LOWER_BOUND_MS)); 
+            sleep_ms(pause_ms);
         }
 
         // Reverse direction
